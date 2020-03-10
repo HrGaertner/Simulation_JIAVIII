trying to fix an overflow

car.py
@@ -45,8 +45,10 @@ class Car():
             self.distance = 0.0
         else:
             print(self.id, self.v, float(self.streets[self.current][self.next]["max_v"]))
-            a_free = self.a*(1-(self.v / float(self.streets[self.current][self.next]["max_v"]))**4)# See the paper of Martin Treiber
-
+            try:
+                a_free = self.a*(1-(self.v / float(self.streets[self.current][self.next]["max_v"]))**4)# See the paper of Martin Treiber
+            except:
+                print('hi')
             if not self.streets[self.current][self.next]['cars'][self.next] == [self]:  # Checks whether there is another car on the street
                 next_car = False
                 event = False
@@ -57,11 +59,11 @@ class Car():
                         event = True
                 if not next_car:
                     self.v += a_free * self.time_step
-                    self.distance += self.v
+                    self.distance += self.v * self.time_step
                 else:
                     gap_s = next_car.distance - self.distance
                     #if gap_s == 0:
-                    gap_s = np.max(gap_s, 0.1)
+                    gap_s = max(gap_s, 0.1)
                     delta_v = self.v - next_car.v
                     a_int = -self.a*((self.s_0 + max(0, (self.v*self.T + (self.v*delta_v/2/math.sqrt(self.a*self.b)))))/gap_s)**2  # See at the paper of Martin Treiber
                     dv_dt = a_free + a_int
@@ -69,4 +71,6 @@ class Car():
                     self.distance += self.v * self.time_step
             else:
                 self.v += a_free * self.time_step
+                if np.abs(self.v)> 1e50:
+                    print('wtf')
                 self.distance += self.v * self.time_step